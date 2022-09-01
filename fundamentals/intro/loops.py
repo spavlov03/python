5b8868215a764f7a1785d9605b36323c982d241d
Svet Pavlov <pavlov.svetoslav@gmail.com>
2022-09-01 15:05:03 -0700

    for loops

---
+++ fundamentals/intro/loops.py
@@ -0,0 +1,11 @@
+my_list = ["abc", 123, "xyz"]
+for i in range(0, len(my_list)):
+    print(i,my_list[i])
+# output: 0 abc, 1 123, 2 xyz
+    
+# OR 
+
+for v in my_list:
+    print(v)
+# output: abc, 123, xyz
+
