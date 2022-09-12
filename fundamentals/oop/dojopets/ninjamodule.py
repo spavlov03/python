c0260610189bfba783070e03983d514a37e657d8
Svet Pavlov <pavlov.svetoslav@gmail.com>
2022-09-12 14:46:39 -0700

    Dojopets v4

--- fundamentals/oop/dojopets/ninjamodule.py
+++ fundamentals/oop/dojopets/ninjamodule.py
@@ -4,25 +4,36 @@ class Ninja:
     def __init__(self,first_name,last_name,pet,treats,pet_food):
         self.first_name = first_name
         self.last_name = last_name
-        self.pet = petmodule.Pet_class(pet,"Dog","jump")
+        self.pet = petmodule.Pet_class(pet,"TEST","jump")
         self.treats = treats
         self.pet_food = pet_food
     def walk(self): 
-        
         self.pet.play()
     def feed(self):
         self.pet.eat()
     def bathe(self):
         self.pet.noise()
 
-svet = Ninja("Svet","Pavlov","Gabi","bones","soup")
-print(f"Health before walk is : {svet.pet.health}")
+svet = Ninja("Svet","Pavlov",petmodule.Cats("Gabi","TESTER","strings"),"milk","mice")
+print(f"{svet.pet.name.name} is a {svet.pet.name.type} that likes to eat {svet.treats} and play with {svet.pet.name.tricks} ")
+print(f"{svet.pet.name.name} Health before walk is : {svet.pet.health}")
 svet.walk()
-print(f"Health after walk is : {svet.pet.health}")
-print(f"Energy & Health before feed are : {svet.pet.energy} & {svet.pet.health}")
+print(f"{svet.pet.name.name} Health after walk is : {svet.pet.health}")
+print(f"{svet.pet.name.name} Energy & Health before feed are : {svet.pet.energy} & {svet.pet.health}")
 svet.feed()
-print(f"Energy & Health after feed are : {svet.pet.energy} & {svet.pet.health}")
-print(f"Before bathe")
+print(f"{svet.pet.name.name} Energy & Health after feed are : {svet.pet.energy} & {svet.pet.health}")
+print(f"{svet.pet.name.name} Before bathe")
 svet.bathe()
-print(f"After bathe")
+print(f"{svet.pet.name.name} After bathe")
 
+pavel = Ninja("Pavel","Skende",petmodule.Dogs('Skotch','cat','catch','sticks'),'cats','bones')
+print(f"{pavel.pet.name.name} is a {pavel.pet.name.type} that likes to eat {pavel.treats} and play with {pavel.pet.name.fav_toy} ")
+print(f"{pavel.pet.name.name} Health before walk is : {pavel.pet.health}")
+pavel.walk()
+print(f"{pavel.pet.name.name} Health after walk is : {pavel.pet.health}")
+print(f"{pavel.pet.name.name} Energy & Health before feed are : {pavel.pet.energy} & {pavel.pet.health}")
+pavel.feed()
+print(f"{pavel.pet.name.name} Energy & Health after feed are : {pavel.pet.energy} & {pavel.pet.health}")
+print(f"{pavel.pet.name.name} Before bathe")
+pavel.bathe()
+print(f"{pavel.pet.name.name} After bathe")
--- fundamentals/oop/dojopets/petmodule.py
+++ fundamentals/oop/dojopets/petmodule.py
@@ -13,4 +13,19 @@ class Pet_class:
     def play(self):
         self.health +=5
     def noise(self):
-        print(f"{self.name} sound")
+        print(f"{self.name.name} sound")
+
+class Dogs(Pet_class):
+    def __init__(self, name, type, tricks, fav_toy):
+        super().__init__(name, type, tricks)
+        self.fav_toy = fav_toy
+        self.type = "Dog"
+    def fetch(self): 
+        print("Get the ball boy!")
+
+class Cats(Pet_class):
+    def __init__(self, name, type, tricks):
+        super().__init__(name, type, tricks)
+        self.type = "Cat"
+    def fetch(self):
+        print("here kitty kitty")
