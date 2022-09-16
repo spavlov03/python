aef8cf12f30eb066cf83008055b0a80dea291f95
Svet Pavlov <pavlov.svetoslav@gmail.com>
2022-09-16 16:33:49 -0700

    Hello World V2

--- flask/fundamentals/hello_flask/server.py
+++ flask/fundamentals/hello_flask/server.py
@@ -1,14 +1,16 @@
-from flask import Flask  # Import Flask to allow us to create our app
-app = Flask(__name__)    # Create a new instance of the Flask class called "app"
-@app.route('/')          # The "@" decorator associates this route with the function immediately following
-def hello_world():
-    return 'Hello World!'  # Return the string 'Hello World!' as a response
+from flask import Flask, render_template
+app = Flask(__name__)
+@app.route('/')
+def index():
+    return render_template("index.html")
 @app.route('/success')
 def success():
     return "Success"
 @app.route('/hello/<string:banana>/<int:num>')
 def hello(banana,num):
-    return f"Hello {banana.capitalize() * num}"
-
-if __name__=="__main__":   # Ensure this file is being run directly and not from a different module    
-    app.run(debug=True, port=8000)    # Run the app in debug mode.
+    return render_template("hello.html", banana=banana, num=num)
+@app.route('/<path:path>')
+def catch_all(path):
+    return 'Sorry! No response. Try again.'
+if __name__=="__main__":
+    app.run(debug=True, port=8000)
