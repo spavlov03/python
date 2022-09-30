4b08516bff5e0f8e71e961fbdee96f683e7ab268
Svet Pavlov <pavlov.svetoslav@gmail.com>
2022-09-30 00:49:20 -0700

    Books v2

--- flask_mysql/crud/books/flask_app/controllers/books.py
+++ flask_mysql/crud/books/flask_app/controllers/books.py
@@ -1,6 +1,6 @@
 from flask_app import app
 from flask import render_template, redirect, request,session
-from flask_app.models import book
+from flask_app.models import book,author
 
 @app.route("/books")
 def all_books():
@@ -11,3 +11,19 @@ def add_book():
     print("BOOK DATA IS----",request.form)
     book.Book.new_book(request.form)
     return redirect("/books")
+@app.route("/books/<int:id>")
+def show_book(id):
+    data = {"id":id}
+    one_book = book.Book.get_book_by_id(id) 
+    authors = author.Author.get_all_authors()
+    books_authors = book.Book.get_books_with_author(data)
+    print("book authors are-----",books_authors)
+    return render_template("book.html",one_book=one_book, authors=authors,books_authors=books_authors)
+@app.route("/books/<int:id>/add-fav",methods=["POST"])
+def add_book_fav(id):
+    data = {
+        "author_id": request.form['author_id'], 
+        "book_id": id
+    }
+    book.Book.add_book_fav_author(data)
+    return redirect(f"/books/{id}")
--- flask_mysql/crud/books/flask_app/models/book.py
+++ flask_mysql/crud/books/flask_app/models/book.py
@@ -9,7 +9,7 @@ class Book:
         self.num_of_pages = data["num_of_pages"]
         self.created_at = data["created_at"]
         self.updated_at = data["updated_at"]
-        self.authors = []
+        self.authors_with_fav = []
 
     @classmethod
     def get_all_books(cls):
@@ -27,18 +27,33 @@ class Book:
         print("___ADDING NEW BOOK___",results)
         return results
     @classmethod
+    def get_book_by_id(cls,id):
+        data = {"id":id}
+        query = "SELECT * FROM books WHERE id = %(id)s;"
+        result = connectToMySQL(cls.DB).query_db(query,data)
+        print("___SHOWING BOOK___",result)
+        return result
+
+    @classmethod
     def get_books_with_author(cls,data):
-        query = "SELECT * FROM authors LEFT JOIN favorites ON favorites.author_id = authors.id LEFT JOIN books ON favorites.book_id = books.id WHERE books.id = %(id)s"
+        query = "SELECT * FROM books LEFT JOIN favorites ON favorites.book_id = books.id LEFT JOIN authors ON favorites.author_id = authors.id WHERE books.id = %(id)s"
         result = connectToMySQL(cls.DB).query_db(query,data)
         print("BOOKS")
         book = cls(result[0])
         for row in result: 
+            if row['authors.id'] == None:
+                break
             author_data = {
                 "id": row["authors.id"],
                 "name": row["name"],
                 "created_at":row["authors.created_at"], 
                 "updated_at":row["authors.updated_at"]
             }
-            book.authors.append(author.Author(author_data))
+            book.authors_with_fav.append(author.Author(author_data))
         return book
-    
+    @classmethod
+    def add_book_fav_author(cls,data):
+        query = "INSERT INTO favorites (author_id,book_id) VALUES (%(author_id)s,%(book_id)s);"
+        result = connectToMySQL(cls.DB).query_db(query,data)
+        print("___MAKING NEW FAVORITE AUTHOR___", result)
+        return result
