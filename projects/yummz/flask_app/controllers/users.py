5faa4d048919d1583d721b98768ecb41c492a152
Svet Pavlov <pavlov.svetoslav@gmail.com>
2022-11-14 08:50:28 -0800

    yummz added functionality

--- projects/yummz/flask_app/controllers/users.py
+++ projects/yummz/flask_app/controllers/users.py
@@ -1,8 +1,9 @@
 from flask import render_template, redirect, request, session, flash
 from flask_app import app
 from flask_app.models import user
-
-
+import requests
+from flask_bcrypt import Bcrypt
+bcrypt = Bcrypt(app)
 
 
 @app.route("/")
@@ -14,6 +15,18 @@ def index():
 def login():
   return render_template('login.html')
 
+@app.route("/login-user",methods=["POST"])
+def login_user(): 
+  users = user.User.get_user_by_email(request.form)
+  if not users:
+      flash("Invalid Email/Password","login")
+      return redirect('/login')
+  if not bcrypt.check_password_hash(users.password, request.form['password']):
+      flash("Invalid Email/Password","login")
+      return redirect('/login')
+  session['user_id'] = users.id
+  return redirect("/dashboard")
+
 
 @app.route("/signup")
 def signup():
@@ -23,7 +36,7 @@ def signup():
 @app.route("/register",methods=["POST"])
 def register():
   if not user.User.validate_user(request.form):
-        return redirect("/")
+        return redirect("/signup")
   data = {
             "first_name":request.form['first_name'],
             "last_name":request.form['last_name'],
@@ -33,7 +46,20 @@ def register():
   user_id = user.User.register(data)
   session['user_id'] = user_id
 
-  return redirect("/login")
+  return redirect("/dashboard")
+
+@app.route("/dashboard")
+def dashboard():
+  data = {"id":session['user_id']}
+  logged_user = user.User.get_user_by_id(data)
+  return render_template('dashboard.html',logged_user=logged_user)
+
+@app.route("/logout")
+def logout():
+  session.clear()
+  return redirect('/')
+
+
 
 
 
--- projects/yummz/flask_app/models/user.py
+++ projects/yummz/flask_app/models/user.py
@@ -20,7 +20,46 @@ class User:
 
     @classmethod
     def register(cls, user_data):
-      query = "INSERT INTO users (first_name,last_name,email,password) VALUES (%(first_name)s,%(last_name)s,%(email)s,%(password)s);"
-      result = connectToMySQL(cls.DB).query_db(query, user_data)
-      #print("___ADDING NEW USER___",result)
-      return result
+        query = "INSERT INTO users (first_name,last_name,email,password) VALUES (%(first_name)s,%(last_name)s,%(email)s,%(password)s);"
+        result = connectToMySQL(cls.DB).query_db(query, user_data)
+        #print("___ADDING NEW USER___",result)
+        return result
+    
+    @classmethod
+    def get_user_by_email(cls,data): 
+        query = "SELECT * FROM users WHERE email=%(email)s;"
+        result = connectToMySQL(cls.DB).query_db(query,data)
+        if len(result) < 1: 
+            return False
+        return cls(result[0])
+    
+    @classmethod
+    def get_user_by_id(cls,data):
+        query = "SELECT * FROM users WHERE id=%(id)s;"
+        result = connectToMySQL(cls.DB).query_db(query,data)
+        return result[0]
+
+    @staticmethod
+    def validate_user(user):
+        is_valid = True
+        query = "SELECT * FROM users WHERE email = %(email)s;"
+        result = connectToMySQL(User.DB).query_db(query,user)
+        if len(result) >= 1:
+            flash("Email already taken.","register")
+            is_valid = False
+        if not EMAIL_REGEX.match(user['email']):
+            flash("Invalid email address!","register")
+            is_valid = False
+        if len(user['first_name']) < 2:
+            flash("First name must be at least 2 characters.","register")
+            is_valid = False
+        if len(user['last_name']) < 2:
+            flash("Last name must be at least 2 characters.","register")
+            is_valid = False
+        if not PASS_REGEX.match(user['password']):
+            flash("Password must be at least 8 characters.","register")
+            is_valid = False
+        if user['password'] != user['password_confirm']:
+            flash("Passwords don't match","register")
+            is_valid = False
+        return is_valid
