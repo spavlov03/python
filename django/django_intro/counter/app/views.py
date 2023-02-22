19474b57d1cde5b7da17b49b976181259257a578
Svet Pavlov <pavlov.svetoslav@gmail.com>
2023-02-22 13:06:19 -0800

    merge conflict reverted

---
+++ django/django_intro/counter/app/views.py
@@ -0,0 +1,26 @@
+from django.shortcuts import render, redirect
+
+# Create your views here.
+def index(request): 
+    if 'visits' in request.session: 
+        request.session['visits'] +=1
+    else:
+        request.session['visits'] = 0
+    if 'counter' in request.session: 
+        print('key exists!')
+        request.session['counter'] += 1
+    else: 
+        print("Key Counter does not exist")
+        request.session['counter'] = 0 
+    return render(request,'index.html')
+def clear_ses(request): 
+    del request.session['counter']
+    return redirect('/')
+def double(request): 
+    request.session['counter'] += 1
+    return redirect('/')
+def custom(request):
+    qty = int(request.POST['qty'])
+    
+    request.session['counter'] = request.session['counter'] + qty-1
+    return redirect('/')
