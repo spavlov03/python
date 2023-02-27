d436816da750d99b80acde3067482e4197c4ff4b
Svet Pavlov <pavlov.svetoslav@gmail.com>
2023-02-26 16:33:17 -0800

    many to many django

--- django/django_intro/first_django/surverys/models.py
+++ django/django_intro/first_django/surverys/models.py
@@ -1,1 +1,9 @@
 from django.db import models
+
+from users.models import User
+
+class Survey(models.Model): 
+    name = models.CharField(max_length=255)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    users = models.ManyToManyField(User,related_name='users')
