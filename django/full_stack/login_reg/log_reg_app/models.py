25d4015bee4101c5b52ff4e8e2c36eb9480dc3ed
Svet Pavlov <pavlov.svetoslav@gmail.com>
2023-03-02 16:48:09 -0800

    login reg assignment

---
+++ django/full_stack/login_reg/log_reg_app/models.py
@@ -0,0 +1,9 @@
+from django.db import models
+
+# Create your models here.
+class UserManager(models.Model):
+    def basic_validator(self,postData):
+        errors = {}
+
+class User(models.Model): 
+    
