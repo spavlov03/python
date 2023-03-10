4c89533932dc6c2ed1ab0cdca9344a580bd394a9
Svet Pavlov <pavlov.svetoslav@gmail.com>
2023-03-09 16:37:48 -0800

    added admin panel

--- django/full_stack/login_reg/log_reg_app/admin.py
+++ django/full_stack/login_reg/log_reg_app/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
-
+from .models import User
 # Register your models here.
+class UserAdmin(admin.ModelAdmin): 
+    fields = ("first_name","last_name","email","password")
+    list_display = ["first_name","last_name","email","password"]
+    search_fields = ["first_name","last_name","email"]
+    # ordering = [
+    #     'first_name','last_name"',"email",
+    #     '-first_name','-last_name"',"-email",
+    # ]
+    
+admin.site.register(User,UserAdmin)
